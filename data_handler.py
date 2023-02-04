d6b4c6ca0f3c0b1047cb94668ab99f71d38fdf94
Harry Ni <harryniyh@hotmail.com>
2023-02-04 18:43:42 +0000

    fix code

--- data_handler.py
+++ data_handler.py
@@ -37,9 +37,8 @@ class user_data(object):
         current_date = datetime.today()
         self.inventory["Expired"] = current_date > self.inventory["Expiration Date"]
 
-    def del_data(self, item, quantity, exp_date, status):
-        # TODO: delete specific data from the dataframe
-        pass
+    def del_data(self, item):
+        self.inventory.drop(self.inventory[self.inventory["Item"]==item].index, inplace=True)
 
     def mod_data(self, item, column, new_data):
         # modify existing data, specify item name, column of data to modify, and the new data
