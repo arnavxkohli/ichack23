3c19fef25b2262843f28a0ea58f94553ee672616
Harry Ni <harryniyh@hotmail.com>
2023-02-04 16:42:33 +0000

    add code

--- data_handler.py
+++ data_handler.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import recommender
-
+import datetime
 
 column_names = ["Item","Quantity","Expiration Date", "Status"]
 class user_data(object):
@@ -22,11 +22,19 @@ class user_data(object):
         # TODO: sort the dataframe by exp_date (use date_time module)
 
     def del_data(self, item, quantity, exp_date, status):
+        # TODO: delete specific data from the dataframe
         pass
+
+    def mod_data(self, column, data):
+        # modify existing data
+        self.inventory[column] = data
+    
     def recommend(self):
+        # call the recommender code, leave for now
         pass
 
 a = user_data()
 print(a.inventory)
 a.add_data("apple", 3, "12.12.2022", 0)
+a.add_data("pear", 4, "13.12.2022", 0)
 print(a.inventory)
