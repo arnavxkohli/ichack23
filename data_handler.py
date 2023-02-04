f34b1b60941383fca358dfc76bf50e1c58efb3a7
Harry Ni <harryniyh@hotmail.com>
2023-02-04 18:42:06 +0000

    add del funciton and use_quantity function

--- data_handler.py
+++ data_handler.py
@@ -4,7 +4,6 @@ import recommender
 from datetime import datetime
 
 column_names = ["Item","Quantity","Expiration Date", "Status", "Expired"]
-# status: 0=fresh, -1=off, 1=opened
 class user_data(object):
     def __init__(self):
         self.inventory = pd.DataFrame(columns=column_names)
@@ -38,20 +37,24 @@ class user_data(object):
         current_date = datetime.today()
         self.inventory["Expired"] = current_date > self.inventory["Expiration Date"]
 
-    def del_data(self, item, quantity, exp_date, status):
-        # TODO: delete specific data from the dataframe
-        pass
+    def del_data(self, item):
+        self.inventory.drop(self.inventory[self.inventory["Item"]==item].index, inplace=True)
 
     def mod_data(self, item, column, new_data):
         # modify existing data, specify item name, column of data to modify, and the new data
         self.inventory.loc[self.inventory["Item"]==item, column] = new_data
 
-    # def use_quantity(self, item, quantity, quantity):
-
+    def use_quantity(self, item, quantity_used):
+        quantity = self.inventory.loc[self.inventory["Item"]==item]["Quantity"]
+        new_quantity = quantity-quantity_used
+        if new_quantity > 0:
+            self.mod_data(item, "Quantity", new_quantity)
+        else:
+            self.del_data(item)
     
     def recommend(self):
         # call the recommender API code
-        input = self.inventory.head(5)["Item"].to_list()
+        # input = self.inventory.head(5)["Item"].to_list()
 
 
 a = user_data()
