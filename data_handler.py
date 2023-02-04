9013ef459ea3484a05e933571d0796970a41b1e2
Harry Ni <harryniyh@hotmail.com>
2023-02-04 17:47:21 +0000

    modify code

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,5 @@
 # from sklearn import LinearRegression
 import pandas as pd
-import numpy as np
 import recommender
 import datetime
 
@@ -8,6 +7,7 @@ column_names = ["Item","Quantity","Expiration Date", "Status"]
 class user_data(object):
     def __init__(self):
         self.inventory = pd.DataFrame(columns=column_names)
+        # self.inventory.set_index('Item', inplace=True)
 
     def add_data(self, item, quantity, exp_date, status = 0):
         # code to add new item to the dataframe
@@ -16,25 +16,34 @@ class user_data(object):
         "Quantity": [quantity],
         "Expiration Date": [exp_date],
         "Status": [status]})
+        self.inventory = self.inventory.append(tmp, ignore_index=True)
+        self.sort()
 
-        self.inventory = self.inventory.append(tmp)
+    def sort(self):
+        # sort the dataframe by exp_date (use date_time module)
+        self.inventory['Expiration Date'] = pd.to_datetime(self.inventory['Expiration Date'])
+        self.inventory.sort_values(by='Expiration Date', inplace = True)
 
-        # TODO: sort the dataframe by exp_date (use date_time module)
 
     def del_data(self, item, quantity, exp_date, status):
         # TODO: delete specific data from the dataframe
         pass
 
-    def mod_data(self, column, data):
-        # modify existing data
-        self.inventory[column] = data
+    def mod_data(self, item, column, new_data):
+        # modify existing data, specify item name, column of data to modify, and the new data
+        self.inventory.loc[self.inventory["Item"]==item, column] = new_data
+
+    # def use_quantity(self, item, , ):
+
     
     def recommend(self):
-        # call the recommender code, leave for now
-        pass
+        # call the recommender API code
+        input = self.inventory.head(5)["Item"].to_list()
+
 
 a = user_data()
+a.add_data("apple", 3, "15/11/2022", 0)
+a.add_data("pear", 4, "13/8/2022", 0)
 print(a.inventory)
-a.add_data("apple", 3, "12.12.2022", 0)
-a.add_data("pear", 4, "13.12.2022", 0)
+a.mod_data("apple", "Item", "cherry")
 print(a.inventory)
